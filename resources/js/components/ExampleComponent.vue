c3238df28cd9e10b4820d8fd0c28211eea253d68
NagamineHikaru <nagamine@saltyster.com>
2020-08-12 23:49:09 +0900

    開発環境構築（laravel+vuetify）

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app>
+        <v-content>
+            <v-container fluid fill-height>
+                <v-layout wrap>
+                    <v-btn color='primary'>Button</v-btn>
+                </v-layout>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
