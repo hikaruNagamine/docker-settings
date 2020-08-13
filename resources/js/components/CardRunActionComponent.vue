347e083d7323f76a31326f5ac40f96c5e5e125b1
NagamineHikaru <nagamine@saltyster.com>
2020-08-13 15:08:08 +0900

    update

---
+++ resources/js/components/CardRunActionComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-content>
+    <h2 class="action_title">CARD : Run Action</h2>
+  </v-content>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name:"CardRunAction",
+});
+</script>
+
+<style scoped>
+.action_title {
+  text-align: center;
+}
+</style>
--- resources/js/components/ChooseActionComponent.vue
+++ resources/js/components/ChooseActionComponent.vue
@@ -2,17 +2,21 @@
   <v-content>
     <h2 class="action_title">Choose Action</h2>
     <card-copy-action />
+    <card-run-action />
   </v-content>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import CardCopyAction from "./CardCopyActionComponent.vue";
+import CardRunAction from "./CardRunActionComponent.vue";
+
 
 export default Vue.extend({
   name:"ChooseAction",
   components: {
     CardCopyAction,
+    CardRunAction,
   },
 });
 </script>
