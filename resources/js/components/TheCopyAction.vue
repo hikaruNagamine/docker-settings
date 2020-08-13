51cf2d2fbcb695613e53f3c8394402881636705c
mkXultra <eeapprenctice@gmail.com>
2020-08-13 13:35:24 +0900

    add copy action components

---
+++ resources/js/components/TheCopyAction.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-content>
+    <h2 class="action_title">Copy file or Directory</h2>
+    <base-input-a-to-b />
+  </v-content>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import BaseInputAToB from "./BaseInputAToB.vue";
+
+export default Vue.extend({
+  name:"TheCopyAction",
+  components: {
+    BaseInputAToB,
+  },
+});
+</script>
+
+<style scoped>
+.action_title {
+  text-align: center;
+}
+</style>
