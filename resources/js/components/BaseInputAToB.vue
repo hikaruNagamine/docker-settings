8af499e3033097ad9cb1548443032f388a8e18bc
mkXultra <eeapprenctice@gmail.com>
2020-08-13 13:26:17 +0900

    baseコンポーネントの実装

--- resources/js/components/BaseInputAToB.vue
+++ resources/js/components/BaseInputAToB.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <p>aiueo</p>
-  </div>
+  <v-container class="grey lighten-5">
+    <v-row justify="center" align="center" style="height: 250px;">
+      <v-row justify="center" align="end">
+        <v-col lg="2">
+          <v-text-field label="A input" :rules="rules" hide-details="auto"></v-text-field>
+        </v-col>
+        <v-col md="auto" align="end">to</v-col>
+        <v-col lg="2">
+          <v-text-field label="B input" :rules="rules" hide-details="auto"></v-text-field>
+        </v-col>
+      </v-row>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -10,13 +20,17 @@ import Vue from "vue";
 export default Vue.extend({
   name: "BaseInputAToB",
   data: () => ({
-    test:'test'
+    test: "test",
+    rules: [(value) => !!value || "必ず入力してください"],
   }),
 
   methods: {
     format() {
       return "test";
-    }
-  }
+    },
+  },
 });
 </script>
+
+<style scoped>
+</style>
