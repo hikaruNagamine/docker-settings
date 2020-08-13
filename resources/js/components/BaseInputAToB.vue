d89985cb51269d3ba16a7f3d56b45d4b7c83b205
mkXultra <eeapprenctice@gmail.com>
2020-08-13 11:58:20 +0900

    use Base components

---
+++ resources/js/components/BaseInputAToB.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>aiueo</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "BaseInputAToB",
+  data: () => ({
+    test:'test'
+  }),
+
+  methods: {
+    format() {
+      return "test";
+    }
+  }
+});
+</script>
